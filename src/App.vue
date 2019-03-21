102651f1ba2125706d0a1d247069ff0f3c210648
Czapkowicz Wojciech <wojciechczapkowicz@gmail.com>
2019-03-21 17:43:47 +0100

    test

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" class="logo">
+    <todoList/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import todoList from './components/todoList'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    todoList
+  }
+}
+</script>
+
+<style>
+
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+ /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.logo {
+  display: block;
+  margin: 20px auto;
+  height: 75px;
+}
+
+</style>
---
+++ src/components/todoList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <input type="text" class="todo-input" placeholder="Write your todo">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+    .todo-input {
+        width: 100%;
+        padding: 10px 18px;
+        font-size: 18px;
+        margin-bottom: 16px;
+
+
+        &:focus {
+            outline: 0;
+        }
+    }
+</style>
